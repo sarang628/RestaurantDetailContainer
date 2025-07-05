0198ee5e9c13f3e5aa763990a3d6a41c7e614b37
sryang <sarang628@gmail.com>
2025-07-05 14:51:53 +0800

    update

---
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sarang.torang
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.sarang.torang.ui.theme.RestaurantDetailContailerTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RestaurantDetailContailerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RestaurantNavScreen(restaurantId = 234)
+                }
+            }
+        }
+    }
+}
