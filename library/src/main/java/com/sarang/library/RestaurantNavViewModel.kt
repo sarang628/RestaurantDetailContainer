07192b04872e6a5e10504a3e66e7d0a27cc8a16c
sryang <sarang628@gmail.com>
2025-07-05 14:31:39 +0800

    update

---
+++ library/src/main/java/com/sarang/library/RestaurantNavViewModel.kt
@@ -0,0 +1,29 @@
+package com.sarang.torang
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RestaurantNavViewModel @Inject constructor(
+    //val findRestaurantNameByRestaurantIdUseCase: FindRestaurantNameByRestaurantIdUseCase
+) : ViewModel() {
+    var restaurantName : String by mutableStateOf("")
+    val tag = "__RestaurantNavViewModel"
+    fun fetch(restaurantId: Int) {
+        viewModelScope.launch {
+            try {
+                //restaurantName = findRestaurantNameByRestaurantIdUseCase.invoke(restaurantId)
+            } catch (e: Exception) {
+                Log.e(tag, "$e")
+            }
+        }
+    }
+}
