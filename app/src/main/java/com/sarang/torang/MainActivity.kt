4d6a7eb0590d6bf83e12e5e50375337c17058830
sryang <sarang628@gmail.com>
2025-07-09 14:53:00 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -15,6 +15,7 @@ import com.sarang.torang.compose.restaurantdetail.RestaurantOverViewScreen
 import com.sarang.torang.ui.theme.RestaurantDetailContailerTheme
 import com.sarang.torang.compose.type.LocalRestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
+import com.sarang.torang.di.restaurant_detail_container_di.customRestaurantOverviewInRestaurantDetailContainer
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewImageLoader
 import com.sarang.torang.di.restaurant_overview_di.restaurantOverViewRestaurantInfo
 import dagger.hilt.android.AndroidEntryPoint
@@ -33,16 +34,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-        }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    val customRestaurantOverviewInRestaurantDetailContainer : RestaurantOverviewInRestaurantDetailContainer = {
-        CompositionLocalProvider(
-            LocalRestaurantOverViewImageLoader provides restaurantOverViewImageLoader,
-            LocalRestaurantOverviewRestaurantInfo provides restaurantOverViewRestaurantInfo,
-        ) {
-            RestaurantOverViewScreen(restaurantId = 234)
         }
     }
 }
