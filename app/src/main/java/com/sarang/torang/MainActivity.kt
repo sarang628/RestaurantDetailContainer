e522a04639c6f3d3da8704e76282a9a2f32d1c43
sryang <sarang628@gmail.com>
2025-12-18 14:33:32 +0900

    update modules

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -10,20 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberBottomAppBarState
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,17 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.sarang.torang.compose.type.LocalRestaurantGalleryInRestaurantDetailContainer
-import com.sarang.torang.compose.type.LocalRestaurantMenuInRestaurantDetailContainer
-import com.sarang.torang.compose.type.LocalRestaurantOverviewInRestaurantDetailContainer
-import com.sarang.torang.compose.type.LocalRestaurantReviewInRestaurantDetailContainer
-import com.sarang.torang.di.dialogsbox_di.ProvideMainDialog
-import com.sarang.torang.di.restaurant_detail_container_di.customRestaurantGalleryInRestaurantDetailContainer
-import com.sarang.torang.di.restaurant_detail_container_di.customRestaurantMenuInRestaurantDetailContainer
-import com.sarang.torang.di.restaurant_detail_container_di.customRestaurantOverviewInRestaurantDetailContainer
-import com.sarang.torang.di.restaurant_detail_container_di.customRestaurantReviewInRestaurantDetailContainer
 import com.sarang.torang.di.restaurant_detail_container_di.provideRestaurantDetailContainer
 import com.sarang.torang.repository.FindRepository
 import com.sryang.torang.ui.TorangTheme
@@ -110,9 +95,7 @@ class MainActivity : ComponentActivity() {
             sheetContent = { sheetContent.invoke() },
         ) { innerPadding ->
             Box(Modifier.padding(innerPadding)){
-                provideRestaurantDetailContainer(
-                    restaurantId = selectedRestaurant
-                ).invoke()
+                provideRestaurantDetailContainer().invoke(selectedRestaurant)
 
                 floatingButton(modifier = Modifier
                     .align(Alignment.BottomEnd)
--- app/src/main/java/com/sarang/torang/RootNavController.kt
+++ app/src/main/java/com/sarang/torang/RootNavController.kt
@@ -1,20 +1,13 @@
 package com.sarang.torang
 
 class RootNavController {
-    fun profile(i: Int) {
-
-    }
-
-    fun review(i: Int) {
-
-    }
-
-    fun popBackStack() {
-
-    }
-    fun map(i: Int) {
-
-    }
-
+    fun profile(i: Int) {}
+    fun review(i: Int) {}
+    fun popBackStack() {}
+    fun map(i: Int) {}
     fun modReview(): (Int) -> Unit { return  {}}
+    fun emailLogin() {}
+    fun like(reviewId: Int) {}
+    fun imagePager(reviewId: Int, lastPage: Int) {}
+    fun restaurant(restaurantId: Int) {}
 }
